05b573adbcefa24a6fb5805e819a9ee9e0a25dcb
Gonzalopana <gonzalopana7@gmail.com>
2025-11-14 14:47:06 -0300

    registros

--- registro.php
+++ registro.php
@@ -7,7 +7,17 @@ $errores = '';
 $correo = $conexion->real_escape_string(string: $_POST['nombre-usuario']);
 $contrasenia = $conexion->real_escape_string(string: $_POST['contrasenia']);
 
-if (empty($correo) || )
+if (empty($correo) || empty($contrasenia) ){
+    $errores .= "<div class='alert alert-danger'>Por favor, completa todos los campos.</div>";
+} else {
+    $query = $conexion->prepare(query: 'SELECT * FROM usuarios WHERE email = ?');
+    $query->bind_param(types: 's', var: &$correo);
+    $query->execute();
+
+    if ($query->get_result()->num_rows > 0) {
+        $errores .= "<div class='alert alert-danger'>El correo ya esta registrado.</div>";
+    }
+}
 
 
 ?>
