cde5833df92b352a25b27fdec9bb587148277f64
Gonzalopana <gonzalopana7@gmail.com>
2025-11-14 15:26:23 -0300

    Final papa

--- registro.php
+++ registro.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'componentes/conexion.php';
 (global variable) string $contrasenia
 
@@ -18,6 +18,45 @@ if (empty($correo) || empty($contrasenia) ){
         $errores .= "<div class='alert alert-danger'>El correo ya esta registrado.</div>";
     }
 }
+    if(empty($errores)){
+        $contra_hash = password_hash(password: $contrasenia, algo: PASSWORD_BCRYPT);
 
+        $query = $conexion->prepare(query: 'INSERT INTO usuarios (email, contrasenia) VALUES (?,?)');
+        $query->bind_param(types: 'ss', var: &$correo, vars: &$contra_hash);
+        $sentencia = $query->execute();
 
+        $query->close();
+        $conexion->close();
+    
+        if($sentencia){
+            $success = "<div class='alert alert-success'>Registro exitoso. Por favor, inicia sesion. </div>";
+            header(header: 'Location: index.php');
+        } else {
+            $errores = "<div class='alert alert-danger'>Error en BBDD, pruebe mas tarde</div>";
+        }
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agencia de viajes - Login</title>
+</head>
+
+<body>
+    <form method="POST" action="registro.php">
+        <?php require_once 'componentes/comp-form-login.php';?>
+    </form>
+    <div>
+        <p>¿No tienes usuarios? Registrate: <a href="registro.php">aqui</a></p>
+    </div>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
