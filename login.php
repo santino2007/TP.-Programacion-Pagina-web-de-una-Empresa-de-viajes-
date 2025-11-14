1342085858127d6dd1aa7ff267e558d0531d4e1d
Gonzalopana <gonzalopana7@gmail.com>
2025-11-14 13:53:18 -0300

    login

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST" action="login.php">
+        <?php require_once 'comp-form-login.php'; ?>
+    </form>
+    <div>
+        <p>¿No tienes usuario? Registrate: <a </p>
+    </div>
+</body>
+</html>
