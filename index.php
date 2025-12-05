49bd829845d123237fb4cb6c18df77fb7582ec39
Jaack5532 <julianalv2008@gmail.com>
2025-12-05 15:12:02 -0300

    jkhtgrm

--- index.php
+++ index.php
@@ -16,14 +16,15 @@
 
     <style>
         body {
-            background-image: url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1600&q=80');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            height: 100vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
+    background-image: url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1600&q=80');
+    background-size: cover;        
+    background-position: center;   
+    background-repeat: no-repeat;  
+    background-attachment: fixed;  
+    margin: 0;
+}
+
+
         
 
         .navbar {
